27c335fffc4d3e15dbe8b9e62e0ac1a2910fe606
laranginha <metroanuncios@gmail.com>
2019-08-22 02:59:19 -0500

    Version 3.9.9.8

--- classes/uiform-installdb.php
+++ classes/uiform-installdb.php
@@ -200,7 +200,7 @@ class Uiform_InstallDB {
         //insert data
         $uifm_check_total = $wpdb->get_row("SELECT COUNT(*) AS total FROM ".$this->settings, ARRAY_A );
        if( isset($uifm_check_total['total']) && intval($uifm_check_total['total'])===0 ){
-           $sql="INSERT INTO $this->settings VALUES ('3.9.9.6.1', null, null, null, null, null, null, '', '1');";
+           $sql="INSERT INTO $this->settings VALUES ('3.9.9.8', null, null, null, null, null, null, '', '1');";
         $wpdb->query($sql);
        }
         
